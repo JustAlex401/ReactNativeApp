ad042e5862dca86b3f446eb6cc1b64585a8792ee
JustAlex401 <plashko99@mail.ru>
2021-10-12 17:36:10 +0300

    Added redux

--- App.js
+++ App.js
@@ -3,17 +3,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import Navigator from './src/navigation/Navigator';
+import { store, persistor } from './src/redux/store';
 
 EStyleSheet.build({});
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Navigator/>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Navigator/>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </PersistGate>
+    </Provider>
   );
 };
 
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import userReducer from './userReducer';
+
+module.exports = {
+  userReducer
+};  
