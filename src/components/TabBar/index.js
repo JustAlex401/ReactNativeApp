95d4923f1ba4017ee1867df7ab377748dd833ef3
JustAlex401 <plashko99@mail.ru>
2021-10-18 18:16:25 +0300

    Added blur tabbar effect and styles for ios

--- src/components/TabBar/index.js
+++ src/components/TabBar/index.js
@@ -1,14 +1,29 @@
 
 import React from 'react';
-import { Image, SafeAreaView, TouchableOpacity, View} from 'react-native';
+import { Image, Platform, SafeAreaView, TouchableOpacity, View} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import CustomText from '../CustomText';
 import globalStyles from '../../../assets/styles';
 import bottomBarImagesPath from '../../utils/bottomBarImagesPath';
+import { BlurView } from '@react-native-community/blur'
 
 export default TabBar = ({state, descriptors, navigation}) => {
   return (
-    <SafeAreaView edges={['bottom']} style={styles().mainContainer}>
+    <SafeAreaView edges={['bottom']} style={Platform.OS === 'ios' ? styles().mainContainerIos : styles().mainContainerAndroid}>
+      {Platform.OS === 'ios' && 
+        <BlurView
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0, 
+            left: 0,
+            right: 0
+          }}
+          blurType="light"
+          blurAmount={10}
+          reducedTransparencyFallbackColor="white"
+        />
+      }
       <View style={styles().container}>
         {state.routes.map((route , index) => {
             const { options } = descriptors[route.key];
@@ -51,12 +66,18 @@ export default TabBar = ({state, descriptors, navigation}) => {
 };
 
 const styles = (isFocused) => EStyleSheet.create({
-  mainContainer: {
-    backgroundColor: 'white',
+  mainContainerIos: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0
+  },
+  mainContainerAndroid: {
+    backgroundColor: 'white'
   },
   container: {
     flex: 0,
-    height: 60,
+    height: Platform.OS === 'ios' ? 80 : 60,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between'
@@ -65,7 +86,8 @@ const styles = (isFocused) => EStyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: Platform.OS === 'ios' ? -10 : 0
   },
   text: {
     fontSize: 10,
