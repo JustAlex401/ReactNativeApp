862718fe7e273d5e4bda913e8d83efe02b8cb151
JustAlex401 <plashko99@mail.ru>
2021-10-27 14:30:52 +0300

    Added animation for android splash screen

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import SplashScreen from 'react-native-splash-screen';
 import Navigator from './src/navigation/Navigator';
 import { store, persistor } from './src/redux/store';
 
 EStyleSheet.build({});
 
 const App = () => {
-
-  useEffect(() => {
-    SplashScreen.hide();
-  }, [])
 
   return (
     <Provider store={store}>
--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { Platform, StatusBar, View } from 'react-native';
+import AnimatedSplash from "react-native-animated-splash-screen";
 import AppStack from './stack/AppStack';
 import AuthStack from './stack/AuthStack';
 import globalStyles from '../../assets/styles';
@@ -14,29 +15,47 @@ const Stack = createStackNavigator();
 
 const Navigator = () => {
 
+  const [isLoaded, setLoaded] = useState(false);
   const isSignedIn = useAuth();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 3000)
+  }, []);
   
   return (
-    <>
-      <View style={styles.statusBar}>
-        <StatusBar backgroundColor={globalStyles.themeColor}/>
-      </View>
-      <Stack.Navigator>
-        {isSignedIn ? 
-            <Stack.Screen
-              name="App"
-              component={AppStack}
-              options={{headerShown: false}}
-            />
-          :
-            <Stack.Screen
-              name="Auth"
-              component={AuthStack}
-              options={{headerShown: false}}
-            />
-        }
-      </Stack.Navigator>
-    </>
+    <View style={globalStyles.flex}>
+      <AnimatedSplash
+        translucent={false}
+        isLoaded={isLoaded}
+        logoImage={require("../../assets/icons/logo_splash_screen.png")}
+        backgroundColor={"#C71585"}
+        logoHeight={150}
+        logoWidth={150}
+      >
+      <>
+        <View style={styles.statusBar}>
+          <StatusBar backgroundColor={globalStyles.themeColor}/>
+        </View>
+        <Stack.Navigator>
+          {isSignedIn ? 
+              <Stack.Screen
+                name="App"
+                component={AppStack}
+                options={{headerShown: false}}
+              />
+            :
+              <Stack.Screen
+                name="Auth"
+                component={AuthStack}
+                options={{headerShown: false}}
+              />
+          }
+        </Stack.Navigator>
+      </>
+      </AnimatedSplash>
+    </View>
   );
 };
 
